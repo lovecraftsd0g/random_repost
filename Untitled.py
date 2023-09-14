86a9c9c365a07ad0a5668fe4a0adf3de17e8d637
lovecraftsd0g <144786427+lovecraftsd0g@users.noreply.github.com>
2023-09-14 17:32:33 +0700

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hatred will only make you sad")
+
+
+# In[3]:
+
+
+gun  = input("hello tell me a gun joke n****: ")
+print(gun)
+
+
+# In[ ]:
+
+
+
+
